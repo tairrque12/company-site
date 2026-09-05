5ebf14d740385dc6cf99b6921daefa288f514726
Tairrque <tairrque.r.baker.mil@army.mil>
2026-09-04 22:51:09 -0500

    added comments

--- frontend/src/experience/careers/clients/CareersClient.ts
+++ frontend/src/experience/careers/clients/CareersClient.ts
@@ -1,1 +1,11 @@
-export {};
+//THIS FILE GOES AND GET EVERY DEPARTMENT FROM THE BACKEND
+
+import type { Department } from "@/experience/careers/types/department";
+
+//Promise - means it will hand back list of departments eventually.
+export async function getAllDepartments(): Promise<Department[]> {
+    //GO GET CAREERS - HAVE IT MATCH CONTROLLER ON BACKEND
+    const response = await fetch("/api/careers");
+    //SEND BACK RAW DATA
+    return response.json();
+}
